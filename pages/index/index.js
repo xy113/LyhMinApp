e525b06f2b60696bbe88cc7f49ee4a3e4b8bc108
xy113 <songdewei@163.com>
2018-06-07 11:46:00 +0800

    准备工作，接口封装，自动登录

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,149 +1,104 @@
-Page({
-  data: {
-    background: ['demo-text-1', 'demo-text-2', 'demo-text-3'],
-    imgs: [
-      { url:'https://lyh.songdewei.com/storage/image/2018/05/kN9Sd7g0qDauXAobSkSLH06vpTRHQlIZvE5OH9G2.jpeg'},
-      { url: 'https://lyh.songdewei.com/storage/image/2018/05/ahLyyicz04EcWEX8yB4PCIjRQkUeWkKJe3EpGpgp.jpeg' }
-    ],
-    server: "https://lyh.songdewei.com",
-    news: [
-      { title: '一篇文章', createdTime: '2018-06-05', image: 'image/2018/03/Ujf2hWIxxZXtBQTXl6YsLsAzddShS7T1UuCbetGr.jpeg', view_num: '0', comment_num:'0'},
-      { title: '一篇文章', createdTime: '2018-06-05', image: 'image/2018/03/Ujf2hWIxxZXtBQTXl6YsLsAzddShS7T1UuCbetGr.jpeg', view_num: '0', comment_num: '0' },
-      { title: '一篇文章', createdTime: '2018-06-05', image: 'image/2018/03/Ujf2hWIxxZXtBQTXl6YsLsAzddShS7T1UuCbetGr.jpeg', view_num: '0', comment_num: '0' },
-    ],
-    indicatorDots: true,
-    vertical: false,
-    autoplay: true,
-    circular: true,
-    interval: 5000,
-    duration: 500,
-    previousMargin: 0,
-    nextMargin: 0,
-    menus: [
-      {
-        title: '盘联新闻',
-        img: '../../asset/img/mobile/news.png',
-        url: '../index/index'
-      },
-      {
-        title: '部门招新',
-        img: '../../asset/img/mobile/zhaoxin.png',
-        url: '../index/index'
-      },
-      {
-        title: '活动招募',
-        img: '../../asset/img/mobile/zhaomu.png',
-        url: '../index/index'
-      },
-      {
-        title: '工作机会',
-        img: '../../asset/img/mobile/zhaopin.png',
-        url: '../index/index'
-      },
-      {
-        title: '加入联谊会',
-        img: '../../asset/img/mobile/jiaru.png',
-        url: '../index/index'
-      },
-      {
-        title: '联谊会达人',
-        img: '../../asset/img/mobile/daren.png',
-        url: '../index/index'
-      },
-      {
-        title: '合作伙伴',
-        img: '../../asset/img/mobile/hezuo.png',
-        url: '../index/index'
-      },
-      {
-        title: '组织架构',
-        img: '../../asset/img/mobile/zuzhi.png',
-        url: '../index/index'
-      }
-    ]
-  },
-  onLoad: function () {
-    // this.getImgs()
-    // this.getNews()
-  },
-  changeProperty: function (e) {
-    var propertyName = e.currentTarget.dataset.propertyName
-    var newData = {}
-    newData[propertyName] = e.detail.value
-    this.setData(newData)
-  },
-  changeIndicatorDots: function (e) {
-    this.setData({
-      indicatorDots: !this.data.indicatorDots
-    })
-  },
-  changeAutoplay: function (e) {
-    this.setData({
-      autoplay: !this.data.autoplay
-    })
-  },
-  intervalChange: function (e) {
-    this.setData({
-      interval: e.detail.value
-    })
-  },
-  durationChange: function (e) {
-    this.setData({
-      duration: e.detail.value
-    })
-  },
-  getImgs: function () {
-    var the = this
-    wx.request({
-      url: getApp().globalData.servers + '/getImgs',
-      data: {
-        currentPage: 2
-      },
-      success: function (res) {
-        // console.log(res)
-        var data = res.data.data
-        the.setData({
-          imgs: data
-        })
-        // console.log(the.data.imgs)
-      }
-    })
-  },
-  turnTime(t) {
-    var time = new Date(t)
-    var now = new Date()
-    var res = ''
-    if (now.getFullYear() != time.getFullYear()) {
-      res = time.getFullYear() + '-' + (time.getMonth() < 9 ? ('0' + (time.getMonth() + 1)) : (time.getMonth() + 1)) + '-' + time.getDate()
-    } else {
-      if (now.getMonth() != time.getMonth()) {
-        res = (time.getMonth() < 9 ? ('0' + (time.getMonth() + 1)) : (time.getMonth() + 1)) + '-' + time.getDate()
-      } else {
-        res = (now - time) / 86400000 + '天前'
-      }
-    }
-    return res
+const Util = require('../../utils/util');
+const moment = require('../../utils/moment.min');
 
-  },
-  getNews: function (page = 1) {
-    var the = this
-    wx.request({
-      url: getApp().globalData.servers + '/getNews',
-      data: {
-        currentPage: page
-      },
-      success: function (res) {
-        console.log(res)
-        var data = res.data.data
-        for (var i = 0; i < data.length; i++) {
-          console.log(data[i].created_at)
-          data[i].createdTime = the.turnTime(data[i].created_at * 1000)
-        }
-        the.setData({
-          news: data
+let self;
+Page({
+    data: {
+        background: ['demo-text-1', 'demo-text-2', 'demo-text-3'],
+        imgs: [],
+        news: [],
+        indicatorDots: true,
+        vertical: false,
+        autoplay: true,
+        circular: true,
+        interval: 5000,
+        duration: 500,
+        previousMargin: 0,
+        nextMargin: 0,
+        menus: [
+            {
+                title: '盘联新闻',
+                img: '../../asset/img/mobile/news.png',
+                url: '../index/index'
+            },
+            {
+                title: '部门招新',
+                img: '../../asset/img/mobile/zhaoxin.png',
+                url: '../index/index'
+            },
+            {
+                title: '活动招募',
+                img: '../../asset/img/mobile/zhaomu.png',
+                url: '../index/index'
+            },
+            {
+                title: '工作机会',
+                img: '../../asset/img/mobile/zhaopin.png',
+                url: '../index/index'
+            },
+            {
+                title: '加入联谊会',
+                img: '../../asset/img/mobile/jiaru.png',
+                url: '../index/index'
+            },
+            {
+                title: '联谊会达人',
+                img: '../../asset/img/mobile/daren.png',
+                url: '../index/index'
+            },
+            {
+                title: '合作伙伴',
+                img: '../../asset/img/mobile/hezuo.png',
+                url: '../index/index'
+            },
+            {
+                title: '组织架构',
+                img: '../../asset/img/mobile/zuzhi.png',
+                url: '../index/index'
+            }
+        ]
+    },
+    onLoad: function () {
+        self = this;
+        Util.request('/block/batchget_item', {block_id:10}).then(response => {
+            console.log(response);
+            this.setData({
+                imgs:response.data.items
+            });
+        });
+
+        Util.request('/post/batchget_item', {count:10}).then(response => {
+            let news = response.data.items;
+            news.forEach((item) => {
+                item.formatted_time = moment(new Date(new Date(item.created_at * 1000))).format('MM-DD')
+            });
+            this.setData({news});
+        });
+    },
+    changeProperty: function (e) {
+        var propertyName = e.currentTarget.dataset.propertyName
+        var newData = {}
+        newData[propertyName] = e.detail.value
+        this.setData(newData)
+    },
+    changeIndicatorDots: function (e) {
+        this.setData({
+            indicatorDots: !this.data.indicatorDots
         })
-        console.log(the.data.news)
-      }
-    })
-  }
-})
+    },
+    changeAutoplay: function (e) {
+        this.setData({
+            autoplay: !this.data.autoplay
+        })
+    },
+    intervalChange: function (e) {
+        this.setData({
+            interval: e.detail.value
+        })
+    },
+    durationChange: function (e) {
+        this.setData({
+            duration: e.detail.value
+        })
+    },
+});
--- utils/util.js
+++ utils/util.js
@@ -1,19 +1,129 @@
 const formatTime = date => {
-  const year = date.getFullYear()
-  const month = date.getMonth() + 1
-  const day = date.getDate()
-  const hour = date.getHours()
-  const minute = date.getMinutes()
-  const second = date.getSeconds()
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    const hour = date.getHours();
+    const minute = date.getMinutes();
+    const second = date.getSeconds();
 
-  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
-}
+    return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+};
 
 const formatNumber = n => {
-  n = n.toString()
-  return n[1] ? n : '0' + n
-}
+    n = n.toString();
+    return n[1] ? n : '0' + n
+};
+
+const request = (path, data = {}, method = "GET") => {
+    return new Promise((resolve, reject) => {
+        wx.request({
+            url: 'https://lyh.songdewei.com/minapp'+path,
+            data: data,
+            method: method,
+            header: {
+                'Content-Type': 'application/json',
+                'X-Token': wx.getStorageSync('token')
+            },
+            success: (res) => {
+                if (res.statusCode === 200) {
+
+                    if (res.data.errcode === 401) {
+                        //需要登录后才可以操作
+                        wx.showModal({
+                            title: '',
+                            content: '请先登录',
+                            success: (res) => {
+                                if (res.confirm) {
+                                    wx.switchTab({
+                                        url: '/pages/ucenter/index/index'
+                                    });
+                                }
+                            }
+                        });
+                    } else {
+                        resolve(res.data);
+                    }
+                } else {
+                    reject(res.errMsg);
+                }
+
+            },
+            fail: (err) => {
+                reject(err);
+                console.log(err)
+            }
+        })
+    });
+};
+
+/**
+ * 检查微信会话是否过期
+ */
+const checkSession = () => {
+    return new Promise((resolve, reject) => {
+        wx.checkSession({
+            success: () => {
+                resolve(true);
+            },
+            fail: () => {
+                reject(false);
+            }
+        })
+    });
+};
+
+/**
+ * 调用微信登录
+ */
+const login = () => {
+    return new Promise((resolve, reject) => {
+        wx.login({
+            success: (res) => {
+                if (res.code) {
+                    //登录远程服务器
+                    console.log(res);
+                    resolve(res);
+                } else {
+                    reject(res);
+                }
+            },
+            fail: (err) => {
+                reject(err);
+            }
+        });
+    });
+};
+
+const getUserInfo = () => {
+    return new Promise((resolve, reject) => {
+        // 获取用户信息
+        wx.getSetting({
+            success: res => {
+                if (res.authSetting['scope.userInfo']) {
+                    // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+                    wx.getUserInfo({
+                        success: res => {
+                            console.log(res);
+                            resolve(res);
+                        },
+                        fail:error => {
+                            reject(error);
+                        }
+                    });
+                }
+            },
+            fail: error => {
+                console.log(error);
+                reject(error);
+            }
+        });
+    });
+};
 
 module.exports = {
-  formatTime: formatTime
-}
+    formatTime,
+    request,
+    checkSession,
+    login,
+    getUserInfo
+};
